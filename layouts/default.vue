31f31c2c5d759ddd12dcffe4c7aab58f9ca63839
Feihong Hsu <feihong.hsu@gmail.com>
2017-02-09 14:01:09 -0600

    Add styling to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,22 @@
     <header>
       This is a cool header!
     </header>
+
     <nuxt/>
+
     <footer>
       This is a cool footer!
     </footer>
   </div>
 </template>
+
+<style>
+header {
+  padding: 1rem 0;
+  border-bottom: 1px dashed gray;
+}
+footer {
+  padding: 1rem 0;
+  border-top: 1px dashed gray;
+}
+</style>
