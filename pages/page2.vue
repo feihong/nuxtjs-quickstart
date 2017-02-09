c8a29fea91f6eefcd18ff438929fa127b3ed4f9d
Feihong Hsu <feihong.hsu@gmail.com>
2017-02-09 14:32:38 -0600

    Write the content for page 2 using pug

--- pages/page2.vue
+++ pages/page2.vue
@@ -1,7 +1,12 @@
-<template>
-  <div>
-    <h1>This is Page 2</h1>
-  </div>
+<template lang='pug'>
+  div
+    h1 This is Page 2
+    p This page was written using #[a(href='https://pugjs.org', target='_blank') Pug]
+    p.highlight.
+      This is a block
+      of text that is
+      a bit long
+      and it will turn out highlighted.
 </template>
 
 <script>
@@ -12,3 +17,9 @@ export default {
   }
 }
 </script>
+
+<style>
+p.highlight {
+  background-color: palegoldenrod;
+}
+</style>
