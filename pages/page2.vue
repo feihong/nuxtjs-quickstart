51ec9c965850de6e671a3c1dbe2fb9c8d58fe3ce
Feihong Hsu <feihong.hsu@gmail.com>
2017-02-09 14:48:59 -0600

    Style page 2 using stylus

--- pages/page2.vue
+++ pages/page2.vue
@@ -1,12 +1,14 @@
 <template lang='pug'>
   div
     h1 This is Page 2
+    p Go back #[nuxt-link(to='/') Home] (I used #[code nuxt-link] here).
     p This page was written using #[a(href='https://pugjs.org', target='_blank') Pug]
     p.highlight.
       This is a block
       of text that is
       a bit long
       and it will turn out highlighted.
+    p A message from Vue: #[span.message {{ message }}]
 </template>
 
 <script>
@@ -14,12 +16,20 @@ export default {
   // It's OK to use an object for head instead of a function.
   head: {
     title: 'Page Two'
+  },
+  data() {
+    return {message: 'Cool message from the data() method'}
   }
 }
 </script>
 
-<style>
-p.highlight {
-  background-color: palegoldenrod;
-}
+<style lang='stylus'>
+p.highlight
+  background-color: palegoldenrod
+span.message
+  color: steelblue
+  font-weight: bold
+  font-size: 1.2rem
+  font-family: monospace
+  text-decoration: underline
 </style>
