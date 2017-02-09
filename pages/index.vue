c17aecdb2843d2eedd6c81704603d45698213d74
Feihong Hsu <feihong.hsu@gmail.com>
2017-02-09 13:57:25 -0600

    Add some dynamic behavior on the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,45 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <h1>Hello world!</h1>
+
+    <p>{{ greeting }}</p>
+    <p>
+      <button v-on:click='updateGreeting'>Click me!</button>
+    </p>
+  </div>
 </template>
 
 <script>
+const greetings = [
+    'Hello World',
+    'Hola Mundo',
+    'ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ ਦੁਨਿਆ',
+    'こんにちは世界',
+    '你好世界',
+    'Përshendetje Botë',
+    'مرحبا بالعالم',
+    'Բարեւ, աշխարհ',
+    'হ্যালো দুনিয়া',
+    'Saluton mondo',
+    'გამარჯობა მსოფლიო',
+]
+
+function getGreeting() {
+  let index = Math.floor(Math.random() * greetings.length)
+  return greetings[index]
+}
+
 export default {
+  data() {
+    return {greeting: getGreeting()}
+  },
   head() {
     return {title: 'Hello World!'}
+  },
+  methods: {
+    updateGreeting() {
+      this.greeting = getGreeting()
+    }
   }
 }
 </script>
