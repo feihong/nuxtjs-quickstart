fba9d39524d9bb348180692daf3a978ca677bbb3
Feihong Hsu <feihong.hsu@gmail.com>
2017-02-09 13:47:24 -0600

    Add a default layout

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <header>
+      This is a cool header!
+    </header>
+    <nuxt/>
+    <footer>
+      This is a cool footer!
+    </footer>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>Hello world!</h1>
 </template>
+
+<script>
+export default {
+  head() {
+    return {title: 'Hello World!'}
+  }
+}
+</script>
